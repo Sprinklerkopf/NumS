918e8c75a3fca11ecc1bf99a66b9ad6a40a2643b
sprinklerkopf <david.schwarzbeck@fau.de>
2020-07-05 20:51:03 +0200

    Added Font Interface

--- nums/src/main/scala/visual/OpenGLFont.scala
+++ nums/src/main/scala/visual/OpenGLFont.scala
@@ -1,5 +1,18 @@
 package visual
 
-class OGLChar{
+import java.awt.Font
+import java.awt.FontMetrics
+import java.awt.image.BufferedImage
+import numSmath.Vec3
+import numSmath.Vec2
+
+class OGLChar(c:Char, font:Font){
+    var image:Int = -1 //OpenGL Image
+    def calculate(fm:FontMetrics):BufferedImage = ??? //calculate BufferedImage with drawn char => parallel
+    def toGLImage(img:BufferedImage): Unit = ??? //set Image
+}
+class OGLFont(font:Font){
+    //TODO: generates chars
     
+    def drawString(s:String, pos:Vec2, color:Vec3) = ???
 }
