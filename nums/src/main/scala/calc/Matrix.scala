5fe8a01f88e32235cfd5e5c0209649c938af4c31
sprinklerkopf <david.schwarzbeck@fau.de>
2020-07-13 15:37:44 +0200

    better performance for 2x2 inverse

--- nums/src/main/scala/calc/Matrix.scala
+++ nums/src/main/scala/calc/Matrix.scala
@@ -26,7 +26,7 @@ class Matrix (v:List[List[Double]]){
   def transpose() : Matrix = new Matrix(List.tabulate(n)(j => List.tabulate(m)(i => vals(i)(j))))
   def swapLine(i:Int, j:Int) = if(i < 0 || i > m || j < 0 || j > n) throw new NonExistingElementException("Line does not exist")
     else new Matrix(List.tabulate(m)(k => if(k == i) vals(j) else if(k == j) vals(i) else vals(k)))
-  
+  def shape() = (m,n)
   def toFracString(): String = {
     val s = new StringBuilder("[")
     for(i <- 0 to m-1) for(j <- 0 to n-1) s.append(new Frac(vals(i)(j)) + (if (j != n-1)", " else if(i != m-1) ",\n" else "]"))
@@ -62,59 +62,72 @@ class QuadraticMatrix(vals:List[List[Double]]) extends Matrix(vals){
     det match{
       case None => {
         det = Some(
-        if(vals.size == 2){
-          vals(0)(0)*vals(1)(1) - vals(0)(1)*vals(1)(0)
-        }else if(vals.size == 3){
-          vals(0)(0)*vals(1)(1)*vals(2)(2) + vals(0)(1)*vals(1)(2)*vals(2)(0) + vals(0)(2)*vals(1)(0)*vals(2)(1) -
-          vals(2)(0)*vals(1)(1)*vals(0)(2) - vals(2)(1)*vals(1)(2)*vals(0)(0) - vals(2)(2)*vals(1)(0)*vals(0)(1)
-        }else{ //very unefficient
-          //matrix without first column and i-th row
-          def submatrix(i:Int) = {
-            val row = vals(i)
-            new QuadraticMatrix(vals.filter(p=>p!=row).map(p=>p.drop(1)))
-          } //Laplace says:
-          (0 to vals.size-1).foldLeft(0.0)((sum, i) => sum + vals(i)(0)*(if(i%2==0) 1 else -1)*submatrix(i).determinant())
-        })
+          shape() match{
+            case(2,2) => {
+              vals(0)(0)*vals(1)(1) - vals(0)(1)*vals(1)(0)
+            }
+            case(3,3) => {
+              vals(0)(0)*vals(1)(1)*vals(2)(2) + vals(0)(1)*vals(1)(2)*vals(2)(0) + vals(0)(2)*vals(1)(0)*vals(2)(1) -
+              vals(2)(0)*vals(1)(1)*vals(0)(2) - vals(2)(1)*vals(1)(2)*vals(0)(0) - vals(2)(2)*vals(1)(0)*vals(0)(1)
+            }
+            case(_, _) => { //very unefficient
+              //matrix without first column and i-th row
+              def submatrix(i:Int) = {
+                val row = vals(i)
+                new QuadraticMatrix(vals.filter(p=>p!=row).map(p=>p.drop(1)))
+              } //Laplace says:
+              (0 to vals.size-1).foldLeft(0.0)((sum, i) => sum + vals(i)(0)*(if(i%2==0) 1 else -1)*submatrix(i).determinant())
+            }
+          }
+        )
         det.get
       }
       case Some(value) => det.get
     }
   }
   def inverse():QuadraticMatrix = { //jordan - gauß
-    def swap(a:Array[Array[Double]], i:Int, j:Int){
-      val temp = a(i)
-      a(i) = a(j)
-      a(j) = temp
-    }
-    val d = determinant()
-    if(d == 0) throw new MatrixNotInvertible()
-    val jor = Array.tabulate(m)(i => Array.tabulate(2*m)(j => if(j < m) vals(i)(j) else if(i == j-m) 1f else 0f))
-    //jor => triangular form //Gauß
-    for(i <- 0 to m-2){
-      if(jor(i)(i) == 0){ 
-        var swapped = false
-        for(j <- i+1 to m-1) if(jor(j)(i) != 0 && !swapped){
-          swap(jor, i, j)
-          swapped = true
+    shape() match{
+      case (2, 2) => {
+        val opd = (1/determinant())
+        new QuadraticMatrix(2,Seq(vals(1)(1)*opd, -vals(0)(1)*opd, -vals(1)(0)*opd, vals(0)(0)*opd))
+      }
+      case (_, _) => {
+        def swap(a:Array[Array[Double]], i:Int, j:Int){
+          val temp = a(i)
+          a(i) = a(j)
+          a(j) = temp
         }
-      }
-      for(j <- i+1 to m-1){
-        val pivot = jor(j)(i)/jor(i)(i)
-        for(k <- i to 2*m -1) jor(j)(k) = jor(j)(k)-jor(i)(k)*pivot
+        val d = determinant()
+        if(d == 0) throw new MatrixNotInvertible()
+        val jor = Array.tabulate(m)(i => Array.tabulate(2*m)(j => if(j < m) vals(i)(j) else if(i == j-m) 1f else 0f))
+        //jor => triangular form //Gauß
+        for(i <- 0 to m-2){
+          if(jor(i)(i) == 0){ 
+            var swapped = false
+            for(j <- i+1 to m-1) if(jor(j)(i) != 0 && !swapped){
+              swap(jor, i, j)
+              swapped = true
+            }
+          }
+          for(j <- i+1 to m-1){
+            val pivot = jor(j)(i)/jor(i)(i)
+            for(k <- i to 2*m -1) jor(j)(k) = jor(j)(k)-jor(i)(k)*pivot
+          }
+        }
+        for(i <- 1 to m-1) if(jor(i)(i) == 0) throw new MatrixNotInvertible()
+        //Jordan
+        for(i <- m-1 to 1 by -1){
+          for(j <- i-1 to 0 by -1){
+            val pivot = jor(j)(i)/jor(i)(i)
+            for(k <- i to 2*m-1) jor(j)(k) =jor(j)(k) - jor(i)(k)*pivot
+          }
+        }
+        //norm
+        new QuadraticMatrix(List.tabulate(m)(i => {
+          val pv = jor(i)(i)
+          List.tabulate(m)(j => jor(i)(j+m)/pv)
+        }))
       }
     }
-    for(i <- 1 to m-1) if(jor(i)(i) == 0) throw new MatrixNotInvertible()
-    //Jordan
-    for(i <- m-1 to 1 by -1){
-      for(j <- i-1 to 0 by -1){
-        val pivot = jor(j)(i)/jor(i)(i)
-        for(k <- i to 2*m-1) jor(j)(k) =jor(j)(k) - jor(i)(k)*pivot
-      }
-    }
-    //norm
-    new QuadraticMatrix(List.tabulate(m)(i => {
-      val pv = jor(i)(i)
-      List.tabulate(m)(j => jor(i)(j+m)/pv)
-    }))
   }
 }
